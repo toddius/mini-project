5ca6add6b7e95073c42ad485b6e20915ca8e4914
Mitchell Flood <mitchflood1@gmail.com>
2021-01-16 13:43:37 -0700

    First commit of js file

---
+++ JS.js
@@ -0,0 +1,65 @@
+
+//We need an array of words to choose from
+var words =["baseball","basketball","football","soccer",];
+console.log(words);
+
+
+//We need a button to start the game 
+var button = document.querySelector
+
+
+
+
+// when we press the start button it needs to select a random word from the array
+
+
+
+
+// it needs to know the length of the word 
+
+
+
+
+
+//it has to display spaces for each letter in the word 
+
+
+
+
+// then when you press a letter key it needs to know what key your pressing then matching it to the blank space then displaying the letter in the blank space 
+
+
+
+
+//it needs a boolean (two choices) so if there is no match then nothing happens
+
+
+
+
+
+// and if there is a match the letter shows up in the blank spot
+
+
+
+
+
+// if all matches are made within the timer limit it will display "You Won!" 
+
+
+
+
+
+
+//and if they dont all match it will display "You Lose"
+
+
+
+
+
+
+
+//Whether they win or loose there needs to be a button to start the game over. maybe a return? 
+
+
+//We need the timer 
+
